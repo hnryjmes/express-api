41a1ed73d0d832a3a49ee60d70b95f3282e5f037
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-16 17:17:11 +0000

    Add tslint exception

--- dist/server.js
+++ dist/server.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const server = app_1.default.listen(app_1.default.get("port"), () => {
+    // tslint:disable-next-line: no-console
     console.log("App is running on http://localhost:%d in %s mode", app_1.default.get("port"), app_1.default.get("env"));
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app";
 
 const server = app.listen(app.get("port"), () => {
+// tslint:disable-next-line: no-console
   console.log(
     "App is running on http://localhost:%d in %s mode",
     app.get("port"),
-    app.get("env")
+    app.get("env"),
   );
 });
